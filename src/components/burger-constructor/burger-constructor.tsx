f2c4a8e35ea92fc6207c0fb99e10c4c3ba2176ec
Alex Drobnyy <alexdroba@gmail.com>
2022-08-06 19:32:48 +0600

    update burger-constructor component

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+  ConstructorElement,
+  DragIcon,
+  CurrencyIcon,
+  Button,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './burger-constructor.module.css';
 
@@ -8,32 +13,41 @@ function BurgerConstructor({ data }) {
   const [firstItem, lastItem] = [data[0], data[data.length - 1]];
 
   return (
-    <div className="pt-25 pb-23 pl-4 pr-4">
-      <div className="ml-8 mb-4">
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text={firstItem.name}
-          price={firstItem.price}
-          thumbnail={firstItem.image}
-        />
+    <div className={styles.constructorWrapper}>
+      <div className="mb-10">
+        <div className="ml-8 mb-4">
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text={firstItem.name}
+            price={firstItem.price}
+            thumbnail={firstItem.image}
+          />
+        </div>
+        <ul className={styles.constructor}>
+          {data.map((item, index) => (
+            <li className={styles.constructorItem} key={item._id}>
+              <DragIcon type="primary" />
+              <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
+            </li>
+          ))}
+        </ul>
+        <div className="ml-8">
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={lastItem.name}
+            price={lastItem.price}
+            thumbnail={lastItem.image}
+          />
+        </div>
       </div>
-      <ul className={styles.constructor}>
-        {data.map((item, index) => (
-          <li className={styles.constructorItem} key={item._id}>
-            <DragIcon type="primary" />
-            <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
-          </li>
-        ))}
-      </ul>
-      <div className="ml-8">
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text={lastItem.name}
-          price={lastItem.price}
-          thumbnail={lastItem.image}
-        />
+      <div className={styles.constructorTotal}>
+        <p className="text text_type_digits-medium">610</p>
+        <CurrencyIcon type="primary" />
+        <Button type="primary" size="large">
+          Нажми на меня
+        </Button>
       </div>
     </div>
   );
