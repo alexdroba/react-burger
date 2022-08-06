4823708685b7927f4369240bb96c63128a5077af
Alex Drobnyy <alexdroba@gmail.com>
2022-08-06 16:39:35 +0600

    create ingredients-list component

---
+++ src/components/ingredients-list/ingredients-list.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import IngredientsItem from '../ingredients-item/ingredients-item';
+
+import styles from './ingredients-list.module.css';
+
+function IngredientsList(props) {
+  return (
+    <div className={styles.ingridientsListWrapper}>
+      <p className="text text_type_main-medium mb-6">Булки</p>
+      <ul className={styles.ingridientsList}>
+        {props.data.map((item) => (
+          <li key={item._id}>
+            <IngredientsItem {...item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default IngredientsList;
