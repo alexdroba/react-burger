e34c9581b8e250403d1c7de094e08244e976f8e2
Alex Drobnyy <alexdroba@gmail.com>
2022-08-06 16:40:23 +0600

    update burger-ingredients component

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
 
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import IngredientsList from '../ingredients-list/ingredients-list';
 
-function BurgerIngredients() {
+import styles from './burger-ingredients.module.css';
+
+function BurgerIngredients({ data }) {
   const [current, setCurrent] = React.useState('one');
 
+  const bun = data.filter((item) => item.type === 'bun');
+  const sauce = data.filter((item) => item.type === 'sauce');
+  const main = data.filter((item) => item.type === 'main');
+
+  console.log(data, 'IN');
+  console.log(bun);
+
   return (
-    <div style={{ display: 'flex' }}>
-      <Tab value="one" active={current === 'one'} onClick={setCurrent}>
-        One
-      </Tab>
-      <Tab value="two" active={current === 'two'} onClick={setCurrent}>
-        Two
-      </Tab>
-      <Tab value="three" active={current === 'three'} onClick={setCurrent}>
-        Three
-      </Tab>
+    <div>
+      <p className="text text_type_main-large mt-10 mb-5">Соберите бургер</p>
+      <div className={styles.tabs}>
+        <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+          Булки
+        </Tab>
+        <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+          Соусы
+        </Tab>
+        <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+          Начинки
+        </Tab>
+      </div>
+      <>
+        <IngredientsList title="Булки" data={bun} />
+        <IngredientsList title="Соусы" data={sauce} />
+        <IngredientsList title="Начинки" data={main} />
+      </>
     </div>
   );
 }
