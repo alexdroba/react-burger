28c51b9d34e7b864069c49107939f55758a9150b
Alex Drobnyy <alexdroba@gmail.com>
2022-08-06 13:16:05 +0600

    Update files

---
+++ src/components/app/app.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+
+import styles from './app.module.css';
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <div className={styles.container}>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </div>
+    </>
+  );
+}
+
+export default App;
