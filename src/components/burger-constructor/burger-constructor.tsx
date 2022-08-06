7e2319775cea105e1dcc9f88c7518d482bc54440
Alex Drobnyy <alexdroba@gmail.com>
2022-08-06 18:43:38 +0600

    update burger-constructor component

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-function BurgerConstructor() {
-  return <div>burger-constructor</div>;
+import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './burger-constructor.module.css';
+
+function BurgerConstructor({ data }) {
+  const [firstItem, lastItem] = [data[0], data[data.length - 1]];
+
+  return (
+    <div className="pt-25 pb-23 pl-4 pr-4">
+      <div className="ml-8 mb-4">
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text={firstItem.name}
+          price={firstItem.price}
+          thumbnail={firstItem.image}
+        />
+      </div>
+      <ul className={styles.constructor}>
+        {data.map((item, index) => (
+          <li className={styles.constructorItem} key={item._id}>
+            <DragIcon type="primary" />
+            <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
+          </li>
+        ))}
+      </ul>
+      <div className="ml-8">
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text={lastItem.name}
+          price={lastItem.price}
+          thumbnail={lastItem.image}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default BurgerConstructor;
