b9960c54e413dcdffd21f94e5573106795f1101b
Alex Drobnyy <alexdroba@gmail.com>
2022-08-06 12:50:27 +0600

    Update app-header, App components and index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import AppHeader from './components/app-header/app-header';
+import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
+import BurgerConstructor from './components/burger-constructor/burger-constructor';
 
 import './App.css';
 
@@ -8,6 +10,10 @@ function App() {
   return (
     <>
       <AppHeader />
+      <div className="{styles.container}">
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </div>
     </>
   );
 }
--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -11,23 +11,29 @@ import styles from './app-header.module.css';
 function AppHeader() {
   return (
     <header className={styles.header}>
-      <nav>
-        <div className="header__btn btn-builder btn-icon">
-          <BurgerIcon type="primary" />
-          <span className="text text_type_main-default">Конструктор</span>
-        </div>
-        <div className="header__btn btn-order btn-icon">
-          <ListIcon type="secondary" />
-          <span className="text text_type_main-default text_color_inactive">Лента заказов</span>
-        </div>
-      </nav>
-      <Logo />
-      <nav>
-        <div className="header__btn btn-account btn-icon">
-          <ProfileIcon type="primary" />
-          <span className="text text_type_main-default text_color_inactive">Личный кабинет</span>
-        </div>
-      </nav>
+      <div className={styles.container}>
+        <nav className={styles.navLeftBar}>
+          <div className={styles.buttonIcon}>
+            <BurgerIcon type="primary" />
+            <span className="text text_type_main-default ml-2">Конструктор</span>
+          </div>
+          <div className={styles.buttonIcon}>
+            <ListIcon type="secondary" />
+            <span className="text text_type_main-default text_color_inactive ml-2">
+              Лента заказов
+            </span>
+          </div>
+        </nav>
+        <Logo />
+        <nav className={styles.navRightBar}>
+          <div className={styles.buttonIcon}>
+            <ProfileIcon type="secondary" />
+            <span className="text text_type_main-default text_color_inactive ml-2">
+              Личный кабинет
+            </span>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
