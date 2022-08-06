f58faa8815f9e9c2d5c02384f583f626106f3a30
Alex Drobnyy <alexdroba@gmail.com>
2022-08-06 12:14:36 +0600

    Update app-header component

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './app-header.module.css';
 
 function AppHeader() {
-  return <div>app-header</div>;
+  return (
+    <header className={styles.header}>
+      <nav>
+        <div className="header__btn btn-builder btn-icon">
+          <BurgerIcon type="primary" />
+          <span className="text text_type_main-default">Конструктор</span>
+        </div>
+        <div className="header__btn btn-order btn-icon">
+          <ListIcon type="secondary" />
+          <span className="text text_type_main-default text_color_inactive">Лента заказов</span>
+        </div>
+      </nav>
+      <Logo />
+      <nav>
+        <div className="header__btn btn-account btn-icon">
+          <ProfileIcon type="primary" />
+          <span className="text text_type_main-default text_color_inactive">Личный кабинет</span>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default AppHeader;
