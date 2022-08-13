f6c796bb3e14b54fbc092eb65c7622f7dc70154f
Alex Drobnyy <alexdroba@gmail.com>
2022-08-13 16:10:22 +0600

    update burger-constructor component

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -8,50 +8,72 @@ import {
   Button,
 } from '@ya.praktikum/react-developer-burger-ui-components';
 
+import Modal from '../modal/modal';
+import OrderDetails from '../order-details/order-details';
+
 import styles from './burger-constructor.module.css';
 
 function BurgerConstructor({ data }) {
   const bun = data.filter((item) => item.type === 'bun')[0];
   const ingredients = data.filter((item) => item.type !== 'bun');
 
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <div className={styles.constructorWrapper}>
-      <div className="mb-10">
-        <div className={styles.constructorFirstItem}>
-          <ConstructorElement
-            type="top"
-            isLocked={true}
-            text={bun.name + ' (верх)'}
-            price={bun.price}
-            thumbnail={bun.image}
-          />
+    <>
+      <div className={styles.constructorWrapper}>
+        <div className="mb-10">
+          <div className={styles.constructorFirstItem}>
+            <ConstructorElement
+              type="top"
+              isLocked={true}
+              text={bun.name + ' (верх)'}
+              price={bun.price}
+              thumbnail={bun.image}
+            />
+          </div>
+          <ul className={styles.constructor}>
+            {ingredients.map((item) => (
+              <li className={styles.constructorItem} key={item._id}>
+                <DragIcon type="primary" />
+                <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
+              </li>
+            ))}
+          </ul>
+          <div className={styles.constructorLastItem}>
+            <ConstructorElement
+              type="bottom"
+              isLocked={true}
+              text={bun.name + ' (низ)'}
+              price={bun.price}
+              thumbnail={bun.image}
+            />
+          </div>
         </div>
-        <ul className={styles.constructor}>
-          {ingredients.map((item) => (
-            <li className={styles.constructorItem} key={item._id}>
-              <DragIcon type="primary" />
-              <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
-            </li>
-          ))}
-        </ul>
-        <div className={styles.constructorLastItem}>
-          <ConstructorElement
-            type="bottom"
-            isLocked={true}
-            text={bun.name + ' (низ)'}
-            price={bun.price}
-            thumbnail={bun.image}
-          />
+        <div className={styles.constructorTotal}>
+          <p className="text text_type_digits-medium">610</p>
+          <CurrencyIcon type="primary" />
+          <Button type="primary" size="large" onClick={handleOpenModal}>
+            Оформить заказ
+          </Button>
         </div>
       </div>
-      <div className={styles.constructorTotal}>
-        <p className="text text_type_digits-medium">610</p>
-        <CurrencyIcon type="primary" />
-        <Button type="primary" size="large">
-          Нажми на меня
-        </Button>
+      <div className={styles.constructorModal}>
+        {modalVisible && (
+          <Modal onClose={handleCloseModal}>
+            <OrderDetails />
+          </Modal>
+        )}
       </div>
-    </div>
+    </>
   );
 }
 
