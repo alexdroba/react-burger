71280b20ece26ce0117701cba351c129815f1536
Alex Drobnyy <alexdroba@gmail.com>
2022-08-13 11:56:40 +0600

    update burger-ingredients component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -17,13 +17,13 @@ function BurgerIngredients({ data }) {
     <div>
       <p className="text text_type_main-large mt-10 mb-5">Соберите бургер</p>
       <div className={styles.tabs}>
-        <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+        <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
           Булки
         </Tab>
-        <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+        <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
           Соусы
         </Tab>
-        <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+        <Tab value="main" active={current === 'main'} onClick={setCurrent}>
           Начинки
         </Tab>
       </div>
