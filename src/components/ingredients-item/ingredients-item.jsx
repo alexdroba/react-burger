b8a054a3e0e4acf17f60da554bcb37ba2af7dc1a
Alex Drobnyy <alexdroba@gmail.com>
2022-08-13 11:58:39 +0600

    update ingredients-item component

--- src/components/ingredients-item/ingredients-item.jsx
+++ src/components/ingredients-item/ingredients-item.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import Modal from '../modal/modal';
 
 import styles from './ingredients-item.module.css';
 
 function IngredientsItem(props) {
+  const [state, setState] = useState({
+    modalVisible: false,
+  });
+
+  const handleOpenModal = () => {
+    setState({ ...state, modalVisible: true });
+  };
+
   return (
-    <div className={styles.ingredientsItem}>
-      <img src={props.image} alt={props.name} />
-      <div className={styles.ingredientsItemPrice}>
-        <span className="text text_type_digits-default mr-2">{props.price}</span>
-        <CurrencyIcon type="primary" />
+    <>
+      <div className={styles.ingredientsItem} onClick={handleOpenModal}>
+        <img src={props.image} alt={props.name} />
+        <div className={styles.ingredientsItemPrice}>
+          <span className="text text_type_digits-default mr-2">{props.price}</span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <div className={styles.ingredientsItemName}>
+          <span className="text text_type_main-default">{props.name}</span>
+        </div>
+        <Counter count={1} size="default" />
       </div>
-      <div className={styles.ingredientsItemName}>
-        <span className="text text_type_main-default">{props.name}</span>
-      </div>
-      <Counter count={1} size="default" />
-    </div>
+      <div>{state.modalVisible && <Modal />}</div>
+    </>
   );
 }
 
