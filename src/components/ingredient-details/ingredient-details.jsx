24e84f490c9d2c1cfa793ab8d35b8d920e45dfc4
Alex Drobnyy <alexdroba@gmail.com>
2022-08-13 14:43:39 +0600

    update ingredient-details component

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-function IngredientDetails() {
-  return <div>ingredient-details</div>;
+import styles from './ingredient-details.module.css';
+
+function IngredientDetails(props) {
+  return (
+    <div>
+      <div className={styles.ingredient}>
+        <img src={props.image_large} alt={props.name} />
+        <p className="text text_type_main-medium pt-4 pb-8">{props.name}</p>
+        <ul className={styles.ingredientPropertyList}>
+          <li className={styles.ingredientPropertyItem}>
+            <p className="text text_type_main-default text_color_inactive mb-2">Калории,ккал</p>
+            <p className="text text_type_digits-default text_color_inactive">{props.calories}</p>
+          </li>
+          <li className={styles.ingredientPropertyItem}>
+            <p className="text text_type_main-default text_color_inactive mb-2">Белки, г</p>
+            <p className="text text_type_digits-default text_color_inactive">{props.proteins}</p>
+          </li>
+          <li className={styles.ingredientPropertyItem}>
+            <p className="text text_type_main-default text_color_inactive mb-2">Жиры, г</p>
+            <p className="text text_type_digits-default text_color_inactive">{props.fat}</p>
+          </li>
+          <li className={styles.ingredientPropertyItem}>
+            <p className="text text_type_main-default text_color_inactive mb-2">Углеводы, г</p>
+            <p className="text text_type_digits-default text_color_inactive">
+              {props.carbohydrates}
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default IngredientDetails;
