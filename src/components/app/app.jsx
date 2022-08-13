753c7aea4ae9d6b080794175fc17ec461f970abc
Alex Drobnyy <alexdroba@gmail.com>
2022-08-13 11:01:28 +0600

    update app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,8 +4,6 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
-// import { data } from '../../utils/data';
-
 import styles from './app.module.css';
 
 function App() {
