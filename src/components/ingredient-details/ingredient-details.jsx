37f93c17225fb60eef0af7f952afe523a673d2ed
Alex Drobnyy <alexdroba@gmail.com>
2022-08-13 17:43:21 +0600

    update ingredient-details component

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import IngredientDetailsProperty from '../../ingredient-details-property/ingredient-details-property';
+
 import styles from './ingredient-details.module.css';
 
 function IngredientDetails(props) {
@@ -10,22 +12,16 @@ function IngredientDetails(props) {
         <p className="text text_type_main-medium pt-4 pb-8">{props.name}</p>
         <ul className={styles.ingredientPropertyList}>
           <li className={styles.ingredientPropertyItem}>
-            <p className="text text_type_main-default text_color_inactive mb-2">Калории,ккал</p>
-            <p className="text text_type_digits-default text_color_inactive">{props.calories}</p>
+            <IngredientDetailsProperty title="Калории,ккал" value={props.calories} />
           </li>
           <li className={styles.ingredientPropertyItem}>
-            <p className="text text_type_main-default text_color_inactive mb-2">Белки, г</p>
-            <p className="text text_type_digits-default text_color_inactive">{props.proteins}</p>
+            <IngredientDetailsProperty title="Белки, г" value={props.proteins} />
           </li>
           <li className={styles.ingredientPropertyItem}>
-            <p className="text text_type_main-default text_color_inactive mb-2">Жиры, г</p>
-            <p className="text text_type_digits-default text_color_inactive">{props.fat}</p>
+            <IngredientDetailsProperty title="Жиры, г" value={props.fat} />
           </li>
           <li className={styles.ingredientPropertyItem}>
-            <p className="text text_type_main-default text_color_inactive mb-2">Углеводы, г</p>
-            <p className="text text_type_digits-default text_color_inactive">
-              {props.carbohydrates}
-            </p>
+            <IngredientDetailsProperty title="Углеводы, г" value={props.carbohydrates} />
           </li>
         </ul>
       </div>
