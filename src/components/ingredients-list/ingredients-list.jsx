1573bb4b79b986798ec84f741073074b1418171d
Alex Drobnyy <alexdroba@gmail.com>
2022-08-14 17:01:36 +0600

    update ingredients-list component

--- src/components/ingredients-list/ingredients-list.jsx
+++ src/components/ingredients-list/ingredients-list.jsx
@@ -6,14 +6,14 @@ import IngredientsItem from '../ingredients-item/ingredients-item';
 import { ingredientTypes } from '../../utils/types';
 import styles from './ingredients-list.module.css';
 
-function IngredientsList({ title, data }) {
+function IngredientsList({ title, data, onOpen }) {
   return (
     <div className={styles.ingridientsListWrapper}>
       <p className="text text_type_main-medium mb-6">{title}</p>
       <ul className={styles.ingridientsList}>
         {data.map((item) => (
           <li key={item._id}>
-            <IngredientsItem data={item} />
+            <IngredientsItem data={item} onOpen={onOpen} />
           </li>
         ))}
       </ul>
