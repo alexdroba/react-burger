78539d3e2df319c4556e979b2c577c64a041fe7e
Alex Drobnyy <alexdroba@gmail.com>
2022-08-14 09:52:00 +0600

    update ingredient-details component

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import IngredientDetailsProperty from '../ingredient-details-property/ingredient-details-property';
 
 import styles from './ingredient-details.module.css';
 
-function IngredientDetails(props) {
+function IngredientDetails({ data }) {
   return (
     <div>
       <div className={styles.ingredient}>
-        <img src={props.image_large} alt={props.name} />
-        <p className="text text_type_main-medium pt-4 pb-8">{props.name}</p>
+        <img src={data.image_large} alt={data.name} />
+        <p className="text text_type_main-medium pt-4 pb-8">{data.name}</p>
         <ul className={styles.ingredientPropertyList}>
           <li className={styles.ingredientPropertyItem}>
-            <IngredientDetailsProperty title="Калории,ккал" value={props.calories} />
+            <IngredientDetailsProperty title="Калории,ккал" value={data.calories} />
           </li>
           <li className={styles.ingredientPropertyItem}>
-            <IngredientDetailsProperty title="Белки, г" value={props.proteins} />
+            <IngredientDetailsProperty title="Белки, г" value={data.proteins} />
           </li>
           <li className={styles.ingredientPropertyItem}>
-            <IngredientDetailsProperty title="Жиры, г" value={props.fat} />
+            <IngredientDetailsProperty title="Жиры, г" value={data.fat} />
           </li>
           <li className={styles.ingredientPropertyItem}>
-            <IngredientDetailsProperty title="Углеводы, г" value={props.carbohydrates} />
+            <IngredientDetailsProperty title="Углеводы, г" value={data.carbohydrates} />
           </li>
         </ul>
       </div>
     </div>
   );
 }
+
+IngredientDetails.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default IngredientDetails;
