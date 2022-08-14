4365ee46cab229b8e7608f9f64d2a5eb0dffdbf7
Alex Drobnyy <alexdroba@gmail.com>
2022-08-14 09:06:09 +0600

    update burger-constructor component

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -28,43 +28,41 @@ function BurgerConstructor({ data }) {
   };
 
   return (
-    <>
-      <div className={styles.constructorWrapper}>
-        <div className="mb-10">
-          <div className={styles.constructorFirstItem}>
-            <ConstructorElement
-              type="top"
-              isLocked={true}
-              text={bun.name + ' (верх)'}
-              price={bun.price}
-              thumbnail={bun.image}
-            />
-          </div>
-          <ul className={styles.constructor}>
-            {ingredients.map((item) => (
-              <li className={styles.constructorItem} key={item._id}>
-                <DragIcon type="primary" />
-                <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
-              </li>
-            ))}
-          </ul>
-          <div className={styles.constructorLastItem}>
-            <ConstructorElement
-              type="bottom"
-              isLocked={true}
-              text={bun.name + ' (низ)'}
-              price={bun.price}
-              thumbnail={bun.image}
-            />
-          </div>
+    <div className={styles.constructorWrapper}>
+      <div className="mb-10">
+        <div className={styles.constructorFirstItem}>
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text={bun.name + ' (верх)'}
+            price={bun.price}
+            thumbnail={bun.image}
+          />
         </div>
-        <div className={styles.constructorTotal}>
-          <p className="text text_type_digits-medium">610</p>
-          <CurrencyIcon type="primary" />
-          <Button type="primary" size="large" onClick={handleOpenModal}>
-            Оформить заказ
-          </Button>
+        <ul className={styles.constructor}>
+          {ingredients.map((item) => (
+            <li className={styles.constructorItem} key={item._id}>
+              <DragIcon type="primary" />
+              <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
+            </li>
+          ))}
+        </ul>
+        <div className={styles.constructorLastItem}>
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={bun.name + ' (низ)'}
+            price={bun.price}
+            thumbnail={bun.image}
+          />
         </div>
+      </div>
+      <div className={styles.constructorTotal}>
+        <p className="text text_type_digits-medium">610</p>
+        <CurrencyIcon type="primary" />
+        <Button type="primary" size="large" onClick={handleOpenModal}>
+          Оформить заказ
+        </Button>
       </div>
       <div className={styles.constructorModal}>
         {modalVisible && (
@@ -73,7 +71,7 @@ function BurgerConstructor({ data }) {
           </Modal>
         )}
       </div>
-    </>
+    </div>
   );
 }
 
