1a98c69356dc222323c2fdc641ef06510d8c7afc
Alex Drobnyy <alexdroba@gmail.com>
2022-08-14 17:05:35 +0600

    update ingredients-item component

--- src/components/ingredients-item/ingredients-item.jsx
+++ src/components/ingredients-item/ingredients-item.jsx
@@ -1,49 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 
-import Modal from '../modal/modal';
-import IngredientDetails from '../ingredient-details/ingredient-details';
-
 import { ingredientTypes } from '../../utils/types';
 import styles from './ingredients-item.module.css';
 
-function IngredientsItem({ data }) {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const handleOpenModal = () => {
-    setModalVisible(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalVisible(false);
-  };
+function IngredientsItem({ data, onOpen }) {
+  const handleOpenModal = useCallback(() => {
+    onOpen(data);
+  }, [data]);
 
   return (
-    <>
-      <div className={styles.ingredientsItem} onClick={handleOpenModal}>
-        <img src={data.image} alt={data.name} />
-        <div className={styles.ingredientsItemPrice}>
-          <span className="text text_type_digits-default mr-2">{data.price}</span>
-          <CurrencyIcon type="primary" />
-        </div>
-        <div className={styles.ingredientsItemName}>
-          <span className="text text_type_main-default">{data.name}</span>
-        </div>
-        <Counter count={1} size="default" />
+    <div className={styles.ingredientsItem} onClick={handleOpenModal}>
+      <img src={data.image} alt={data.name} />
+      <div className={styles.ingredientsItemPrice}>
+        <span className="text text_type_digits-default mr-2">{data.price}</span>
+        <CurrencyIcon type="primary" />
       </div>
-      <div className={styles.ingredientsItemModal}>
-        <Modal title="Детали ингредиента" onClose={handleCloseModal} isOpen={modalVisible}>
-          <IngredientDetails data={data} />
-        </Modal>
+      <div className={styles.ingredientsItemName}>
+        <span className="text text_type_main-default">{data.name}</span>
       </div>
-    </>
+      <Counter count={1} size="default" />
+    </div>
   );
 }
 
 IngredientsItem.propTypes = {
   data: PropTypes.shape(ingredientTypes).isRequired,
+  onOpen: PropTypes.func.isRequired,
 };
 
 export default IngredientsItem;
