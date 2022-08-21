7ef19d7adabdc2f7d186e2ef6715beebde6828cd
Alex Drobnyy <alexdroba@gmail.com>
2022-08-21 14:52:24 +0600

    update consts file

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,3 +1,5 @@
-export const API_URL_INGREDIENTS = 'https://norma.nomoreparties.space/api/ingredients';
+const BASE_URL = 'https://norma.nomoreparties.space/api/';
 
-export const API_URL_ORDERS = 'https://norma.nomoreparties.space/api/orders';
+export const BASE_URL_INGREDIENTS = BASE_URL + 'ingredients';
+
+export const BASE_URL_ORDERS = BASE_URL + 'orders';
