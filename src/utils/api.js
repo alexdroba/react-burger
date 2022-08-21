57b7a1ca1927e41f4e7ba11775f4e7d9d0c9a8d9
Alex Drobnyy <alexdroba@gmail.com>
2022-08-21 13:36:28 +0600

    update api file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
-import { API_URL } from './consts';
+import { API_URL_INGREDIENTS, API_URL_ORDERS } from './consts';
 
 export const getIngredientsData = (state, setState) => {
-  return fetch(API_URL)
+  return fetch(API_URL_INGREDIENTS)
     .then((res) => {
       if (res.ok) {
         return res.json();
@@ -9,6 +9,25 @@ export const getIngredientsData = (state, setState) => {
       return Promise.reject(`Ошибка ${res.status}`);
     })
     .then((data) => setState({ ...state, ingredientsData: data.data, isLoading: false }))
+    .catch((e) => {
+      setState({ ...state, hasError: true, isLoading: false });
+    });
+};
+
+export const getOrderData = (state, setState) => {
+  return fetch(API_URL_ORDERS, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    body: JSON.stringify({ ingredients: ['60d3b41abdacab0026a733c6'] }),
+  })
+    .then((res) => {
+      console.log(res);
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка ${res.status}`);
+    })
+    .then((data) => setState({ ...state, order: data.order.number, isLoading: false }))
     .catch((e) => {
       setState({ ...state, hasError: true, isLoading: false });
     });
