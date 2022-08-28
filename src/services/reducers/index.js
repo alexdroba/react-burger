52012ff6d30bed7f19f5e4e46c6d35b137c1d851
Alex Drobnyy <alexdroba@gmail.com>
2022-08-28 12:15:21 +0600

    update reducers

---
+++ src/services/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+const ingredientsInitialState = {
+  isLoading: false,
+  hasError: false,
+  ingredientsData: [],
+};
+
+const rootRedicer = combineReducers({});
