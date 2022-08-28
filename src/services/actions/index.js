e0456ad0ffde1363ab0318e35bf9e7ff3edf8c00
Alex Drobnyy <alexdroba@gmail.com>
2022-08-28 15:32:26 +0600

    update actions

--- src/services/actions/index.js
+++ src/services/actions/index.js
@@ -1,4 +1,4 @@
-import { BASE_URL_INGREDIENTS } from '../../utils/consts';
+import { BASE_URL_INGREDIENTS, BASE_URL_ORDERS } from '../../utils/consts';
 
 export const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST';
 export const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS';
@@ -6,6 +6,10 @@ export const GET_INGREDIENTS_FAILED = 'GET_INGREDIENTS_FAILED';
 
 export const GET_TARGET_INGREDIENT = 'GET_TARGET_INGREDIENT';
 export const DELETE_TARGET_INGREDIENT = 'DELETE_TARGET_INGREDIENT';
+
+export const GET_ORDER_REQUEST = 'GET_ORDER_REQUEST';
+export const GET_ORDER_SUCCESS = 'GET_ORDER_SUCCESS';
+export const GET_ORDER_FAILED = 'GET_ORDER_FAILED';
 
 const checkResponse = (res) => (res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`));
 
@@ -40,5 +44,30 @@ export const getTargetIngredient = (ingredient) => {
 export const deleteTargetIngredient = () => {
   return {
     type: DELETE_TARGET_INGREDIENT,
+  };
+};
+
+export const getOrderData = (idIngredients) => {
+  return function (dispatch) {
+    dispatch({
+      type: GET_ORDER_REQUEST,
+    });
+    fetch(BASE_URL_ORDERS, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      body: JSON.stringify({ ingredients: idIngredients }),
+    })
+      .then(checkResponse)
+      .then((data) => {
+        dispatch({
+          type: GET_ORDER_SUCCESS,
+          order: data.order,
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: GET_ORDER_FAILED,
+        });
+      });
   };
 };
