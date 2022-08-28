da036c6673592430e71a59ef29a01cf37d842d7f
Alex Drobnyy <alexdroba@gmail.com>
2022-08-28 14:05:14 +0600

    update burger-ingredients component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -6,8 +6,6 @@ import IngredientsList from '../ingredients-list/ingredients-list';
 import Modal from '../modal/modal';
 import IngredientDetails from '../ingredient-details/ingredient-details';
 
-import { IngredientsContext } from '../../services/ingredientsContext';
-
 import { getIngredientsData } from '../../services/actions/index';
 
 import styles from './burger-ingredients.module.css';
@@ -16,23 +14,17 @@ function BurgerIngredients() {
   const [currentCategory, setCurrentCategory] = useState('bun');
   const [modalVisible, setModalVisible] = useState(false);
   const [targetIngredient, setTargetIngredient] = useState(null);
-  const { ingredientsData: data } = useContext(IngredientsContext);
 
-  const {
-    isLoading,
-    hasError,
-    ingredientsData: dataNew,
-  } = useSelector((state) => state.ingredients);
-  console.log(dataNew);
+  const { isLoading, hasError, ingredientsData: data } = useSelector((state) => state.ingredients);
   const dispatch = useDispatch();
 
   const categoryBuns = useRef(null);
   const categorySauces = useRef(null);
   const categoryMain = useRef(null);
 
-  const bun = useMemo(() => data.filter((item) => item.type === 'bun'), data);
-  const sauce = useMemo(() => data.filter((item) => item.type === 'sauce'), data);
-  const main = useMemo(() => data.filter((item) => item.type === 'main'), data);
+  const bun = useMemo(() => data.filter((item) => item.type === 'bun'), [data]);
+  const sauce = useMemo(() => data.filter((item) => item.type === 'sauce'), [data]);
+  const main = useMemo(() => data.filter((item) => item.type === 'main'), [data]);
 
   const handleOpenModal = (ingridient) => {
     setModalVisible(true);
@@ -43,15 +35,15 @@ function BurgerIngredients() {
     setModalVisible(false);
   };
 
-  useEffect(() => {
-    if (currentCategory === 'bun') {
-      categoryBuns.current.scrollIntoView({ behavior: 'smooth' });
-    } else if (currentCategory === 'sauce') {
-      categorySauces.current.scrollIntoView({ behavior: 'smooth' });
-    } else {
-      categoryMain.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [currentCategory]);
+  // useEffect(() => {
+  //   if (currentCategory === 'bun') {
+  //     categoryBuns.current.scrollIntoView({ behavior: 'smooth' });
+  //   } else if (currentCategory === 'sauce') {
+  //     categorySauces.current.scrollIntoView({ behavior: 'smooth' });
+  //   } else {
+  //     categoryMain.current.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  // }, [currentCategory]);
 
   useEffect(() => {
     dispatch(getIngredientsData());
@@ -59,26 +51,42 @@ function BurgerIngredients() {
 
   return (
     <div>
-      <p className="text text_type_main-large mt-10 mb-5">Соберите бургер</p>
-      <div className={styles.tabs}>
-        <Tab value="bun" active={currentCategory === 'bun'} onClick={setCurrentCategory}>
-          Булки
-        </Tab>
-        <Tab value="sauce" active={currentCategory === 'sauce'} onClick={setCurrentCategory}>
-          Соусы
-        </Tab>
-        <Tab value="main" active={currentCategory === 'main'} onClick={setCurrentCategory}>
-          Начинки
-        </Tab>
-      </div>
-      <div className={styles.ingredientsList}>
-        <IngredientsList title="Булки" data={bun} onOpen={handleOpenModal} ref={categoryBuns} />
-        <IngredientsList title="Соусы" data={sauce} onOpen={handleOpenModal} ref={categorySauces} />
-        <IngredientsList title="Начинки" data={main} onOpen={handleOpenModal} ref={categoryMain} />
-      </div>
-      <Modal title="Детали ингредиента" onClose={handleCloseModal} isOpen={modalVisible}>
-        <IngredientDetails data={targetIngredient} />
-      </Modal>
+      {isLoading && 'Загрузка...'}
+      {hasError && 'Произошла ошибка!'}
+      {!isLoading && !hasError && data.length && (
+        <>
+          <p className="text text_type_main-large mt-10 mb-5">Соберите бургер</p>
+          <div className={styles.tabs}>
+            <Tab value="bun" active={currentCategory === 'bun'} onClick={setCurrentCategory}>
+              Булки
+            </Tab>
+            <Tab value="sauce" active={currentCategory === 'sauce'} onClick={setCurrentCategory}>
+              Соусы
+            </Tab>
+            <Tab value="main" active={currentCategory === 'main'} onClick={setCurrentCategory}>
+              Начинки
+            </Tab>
+          </div>
+          <div className={styles.ingredientsList}>
+            <IngredientsList title="Булки" data={bun} onOpen={handleOpenModal} ref={categoryBuns} />
+            <IngredientsList
+              title="Соусы"
+              data={sauce}
+              onOpen={handleOpenModal}
+              ref={categorySauces}
+            />
+            <IngredientsList
+              title="Начинки"
+              data={main}
+              onOpen={handleOpenModal}
+              ref={categoryMain}
+            />
+          </div>
+          <Modal title="Детали ингредиента" onClose={handleCloseModal} isOpen={modalVisible}>
+            <IngredientDetails data={targetIngredient} />
+          </Modal>
+        </>
+      )}
     </div>
   );
 }
