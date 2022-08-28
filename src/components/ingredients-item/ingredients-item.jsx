2a8401a89e28156f802d5531747f5cf146738614
Alex Drobnyy <alexdroba@gmail.com>
2022-08-28 22:43:43 +0600

    update ingredients-item component

--- src/components/ingredients-item/ingredients-item.jsx
+++ src/components/ingredients-item/ingredients-item.jsx
@@ -1,5 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { useDrag } from 'react-dnd';
+import { useSelector } from 'react-redux';
 
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -11,8 +13,27 @@ function IngredientsItem({ data, onOpen }) {
     onOpen(data);
   }, [data]);
 
+  const [{ opacity }, dragRef] = useDrag({
+    type: 'ingredient',
+    item: data,
+    collect: (monitor) => ({
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  });
+
+  const { ingredients } = useSelector((state) => state.constructorIngredients);
+
+  const counter = useMemo(
+    () => ingredients.filter((item) => item._id === data._id).length,
+    [ingredients, data],
+  );
+
   return (
-    <div className={styles.ingredientsItem} onClick={handleOpenModal}>
+    <div
+      className={styles.ingredientsItem}
+      onClick={handleOpenModal}
+      ref={dragRef}
+      style={{ opacity }}>
       <img src={data.image} alt={data.name} />
       <div className={styles.ingredientsItemPrice}>
         <span className="text text_type_digits-default mr-2">{data.price}</span>
@@ -21,7 +42,7 @@ function IngredientsItem({ data, onOpen }) {
       <div className={styles.ingredientsItemName}>
         <span className="text text_type_main-default">{data.name}</span>
       </div>
-      <Counter count={1} size="default" />
+      {counter !== 0 && <Counter count={counter} size="default" />}
     </div>
   );
 }
