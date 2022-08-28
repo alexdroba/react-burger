70725b7ddfc82470123fd0ef13e0e581f4c8973e
Alex Drobnyy <alexdroba@gmail.com>
2022-08-28 12:46:37 +0600

    update app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,26 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
-import { rootReducer } from '../../services/reducers/index';
 import { IngredientsContext } from '../../services/ingredientsContext';
 import { getIngredientsData } from '../../utils/api';
 
 import styles from './app.module.css';
 
 function App() {
-  const composeEnhancers =
-    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-      : compose;
-  const enhancer = composeEnhancers(applyMiddleware(thunk));
-  const store = createStore(rootReducer, enhancer);
-
   const [state, setState] = useState({
     isLoading: false,
     hasError: false,
@@ -34,21 +23,19 @@ function App() {
   const { ingredientsData, isLoading, hasError } = state;
 
   return (
-    <Provider store={store}>
-      <IngredientsContext.Provider value={{ ingredientsData }}>
-        <AppHeader />
-        <div className={styles.container}>
-          {isLoading && 'Загрузка...'}
-          {hasError && 'Произошла ошибка!'}
-          {!isLoading && !hasError && ingredientsData.length && (
-            <>
-              <BurgerIngredients />
-              <BurgerConstructor />
-            </>
-          )}
-        </div>
-      </IngredientsContext.Provider>
-    </Provider>
+    <IngredientsContext.Provider value={{ ingredientsData }}>
+      <AppHeader />
+      <div className={styles.container}>
+        {isLoading && 'Загрузка...'}
+        {hasError && 'Произошла ошибка!'}
+        {!isLoading && !hasError && ingredientsData.length && (
+          <>
+            <BurgerIngredients />
+            <BurgerConstructor />
+          </>
+        )}
+      </div>
+    </IngredientsContext.Provider>
   );
 }
 
