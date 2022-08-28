b98a87f216a8995c492438ecae704fff682c9d20
Alex Drobnyy <alexdroba@gmail.com>
2022-08-28 14:52:25 +0600

    update actions

--- src/services/actions/index.js
+++ src/services/actions/index.js
@@ -4,6 +4,9 @@ export const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST';
 export const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS';
 export const GET_INGREDIENTS_FAILED = 'GET_INGREDIENTS_FAILED';
 
+export const GET_TARGET_INGREDIENT = 'GET_TARGET_INGREDIENT';
+export const DELETE_TARGET_INGREDIENT = 'DELETE_TARGET_INGREDIENT';
+
 const checkResponse = (res) => (res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`));
 
 export const getIngredientsData = () => {
@@ -24,5 +27,18 @@ export const getIngredientsData = () => {
           type: GET_INGREDIENTS_FAILED,
         });
       });
+  };
+};
+
+export const getTargetIngredient = (ingredient) => {
+  return {
+    type: GET_TARGET_INGREDIENT,
+    ingredient,
+  };
+};
+
+export const deleteTargetIngredient = () => {
+  return {
+    type: DELETE_TARGET_INGREDIENT,
   };
 };
