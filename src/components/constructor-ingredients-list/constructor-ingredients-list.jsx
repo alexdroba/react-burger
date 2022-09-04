0e60f3debf6ada69c31f83b5d3b1fcf47699753a
Alex Drobnyy <alexdroba@gmail.com>
2022-09-04 13:07:50 +0600

    update constructor-ingredients-list component

--- src/components/constructor-ingredients-list/constructor-ingredients-list.jsx
+++ src/components/constructor-ingredients-list/constructor-ingredients-list.jsx
@@ -1,31 +1,36 @@
 import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import ConstructorIngredientsItem from '../constructor-ingredients-item/constructor-ingredients-item';
 
 import { updateIngredientConstructor } from '../../services/actions/index';
 
+import { ingredientTypes } from '../../utils/types';
 import styles from './constructor-ingredients-list.module.css';
 
 function ConstructorIngredientsList({ data }) {
-  const { ingredients } = useSelector((state) => state.constructorIngredients);
   const dispatch = useDispatch();
 
   const moveCard = useCallback(
     (dragIndex, hoverIndex) => {
-      const dragCard = ingredients[dragIndex];
-      const newCards = [...ingredients];
+      const dragCard = data[dragIndex];
+      const newCards = [...data];
       newCards.splice(dragIndex, 1);
       newCards.splice(hoverIndex, 0, dragCard);
 
       dispatch(updateIngredientConstructor(newCards));
     },
-    [ingredients, dispatch],
+    [data, dispatch],
   );
 
   return data.map((item, index) => (
     <ConstructorIngredientsItem key={item._dndid} index={index} item={item} moveCard={moveCard} />
   ));
 }
+
+ConstructorIngredientsList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape(ingredientTypes)).isRequired,
+};
 
 export default ConstructorIngredientsList;
